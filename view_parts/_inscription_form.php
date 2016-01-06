a47be18c74e2fb00578a9cd7a3c08ec240e4505a
jifou03 <jf.pgrenier@gmail.com>
2016-01-06 08:28:40 -0500

    formulaire

--- view_parts/_inscription_form.php
+++ view_parts/_inscription_form.php
@@ -1,7 +1,21 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: J-F Grenier
- * Date: 2015-12-17
- * Time: 12:25
- */
+var_dump($_POST);
+//validatoion
+?>
+
+
+<form id="formulaire" action="" method="post">
+    <ul>
+        <li><label class="inscription" for="nom">Nom :</label>
+            <input type="text" name="nom" id="nom"/></li>
+        <li><label class="inscription" for="prenom">Prenom :</label>
+            <input type="text" name="prenom" id="prenom"/></li>
+        <li><label class="inscription" for="courriel">Courriel :</label>
+            <input type="text" name="courriel" id="courriel"/></li>
+        <li><label class="inscription" for="nomutilisateur">Nom d'utilisateur :</label>
+            <input type="text" name="nomutilisateur" id="nomutilisateur"/></li>
+        <li><label class="inscription" for="password">Password :</label>
+            <input type="password" name="password" id="password"/></li>
+        <li><input type="submit" id="submit" name="submit" value="Soumettre"/></li>
+    </ul>
+</form>
