215f4afc897f2302e5340ed6e08ff2b732a3a12f
jifou03 <jf.pgrenier@gmail.com>
2016-01-06 21:47:49 -0500

    exreg pour le formulaire

--- view_parts/_inscription_form.php
+++ view_parts/_inscription_form.php
@@ -9,7 +9,7 @@ $prenom_msg = ''; // Message de feedback validation, affiché si non vide
 if (array_key_exists('prenom', $_POST)) {
     $prenom = filter_input(INPUT_POST, 'prenom', FILTER_SANITIZE_STRING);
     // Validation du prenom : min 2 caractères
-    $prenom_ok = (1 === preg_match('/^[A-Za-z]{2,}$/', $prenom));
+    $prenom_ok = (1 === preg_match('/^[A-Za-zç\-]{2,}$/', $prenom));
     if ( ! $prenom_ok) { // Si le prénom n'est pas valide
         $prenom_msg = 'Le prénom ne doit contenir que des lettres (min 2).';
     }
@@ -24,7 +24,7 @@ $nom_ok = false;
 if (array_key_exists('nom', $_POST)) {
     $nom = filter_input(INPUT_POST, 'nom', FILTER_SANITIZE_STRING);
     // Validation du nom: min 2 caracteres
-    $nom_ok = (1 === preg_match('/^[A-Za-z]{2,}$/', $nom));
+    $nom_ok = (1 === preg_match('/^[A-Za-z\-]{2,}$/', $nom));
     var_dump($nom);
     var_dump($nom_ok);
 }
@@ -47,6 +47,13 @@ if (array_key_exists('pseudo', $_POST)) {
     $pseudo = filter_input(INPUT_POST, 'pseudo', FILTER_SANITIZE_STRING);
     // Validation du pseudo : min 4 caractères alpha ou chiffres
     $pseudo_ok = (1 === preg_match('/^[a-zA-Z0-9]{4,}$/', $pseudo));
+    if ($pseudo_ok){
+        //est ce que le pseudo est libre
+        require_once 'db/_user.php';
+        if (username_exists($pseudo)){
+            echo "Le username" . $pseudo . "est déja utilisée";
+        }
+    }
     var_dump($pseudo);
     var_dump($pseudo_ok);
 }
@@ -73,12 +80,12 @@ if ($prenom_ok && $nom_ok && $courriel_ok && $pseudo_ok && $mot_passe_ok) {
            class="<?php echo $in_post && ! $prenom_ok ? 'error' : '';?>"
            value="<?php echo array_key_exists('prenom', $_POST) ? $_POST['prenom'] : '' ?>"/>
     <label for="nom">Nom : </label>
-    <input type="nom" name="nom" id="nom" value="<?php echo array_key_exists('nom', $_POST) ? $_POST['nom'] : '' ?>"/>
+    <input type="text" name="nom" id="nom" value="<?php echo array_key_exists('nom', $_POST) ? $_POST['nom'] : '' ?>"/>
     <label for="courriel">Courriel : </label>
-    <input type="courriel" name="courriel" id="courriel" value="<?php echo array_key_exists('courriel', $_POST) ? $_POST['courriel'] : '' ?>"/>
+    <input type="text" name="courriel" id="courriel" value="<?php echo array_key_exists('courriel', $_POST) ? $_POST['courriel'] : '' ?>"/>
     <label for="pseudo">Pseudo : </label>
-    <input type="pseudo" name="pseudo" id="pseudo" value="<?php echo array_key_exists('pseudo', $_POST) ? $_POST['pseudo'] : '' ?>"/>
+    <input type="text" name="pseudo" id="pseudo" value="<?php echo array_key_exists('pseudo', $_POST) ? $_POST['pseudo'] : '' ?>"/>
     <label for="mot_passe">Password : </label>
-    <input type="mot_passe" name="mot_passe" id="mot_passe" value="<?php echo array_key_exists('mot_passe', $_POST) ? $_POST['mot_passe'] : '' ?>"/>
+    <input type="password" name="mot_passe" id="mot_passe" value="<?php echo array_key_exists('mot_passe', $_POST) ? $_POST['mot_passe'] : '' ?>"/>
     <input type="submit" name="register" id="register" value="S'inscrire"/>
 </form>
